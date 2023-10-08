15aecc63e4f89dc3b2d587d6344568771eda4d17
Walperr <vladbann37@yandex.ru>
2023-10-08 20:28:43 +0700

    removed comment

--- MQTTServer/MQTTServer/Program.cs
+++ MQTTServer/MQTTServer/Program.cs
@@ -7,8 +7,6 @@ using MQTTServer.Services.Implementation;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
